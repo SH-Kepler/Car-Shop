ed5531049db5ba41149cda579ac293ef4c2340ab
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-23 16:08:06 -0300

    feat: Car Domain

---
+++ src/Domains/Car.ts
@@ -0,0 +1,100 @@
+import ICar from '../Interfaces/Icar';
+
+class Car {
+  private id: string | undefined;
+  private model: string;
+  private year: number;
+  private color: string;
+  private status?: boolean;
+  private buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor(
+    {
+      seatsQty,
+      doorsQty,
+      buyValue,
+      status = false,
+      color,
+      year,
+      model,
+      id,
+    }: ICar,
+  ) {
+    this.id = id;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.status = status;
+    this.buyValue = buyValue;
+    this.doorsQty = doorsQty;
+    this.seatsQty = seatsQty;
+  }
+
+  public setId(id: string) {
+    this.id = id;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public setModel(model: string) {
+    this.model = model;
+  }
+
+  public getModel() {
+    return this.model;
+  }
+
+  public setYear(year: number) {
+    this.year = year;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+
+  public setColor(color: string) {
+    this.color = color;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public setStatus(status: boolean) {
+    this.status = status;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public setBuyValue(buyValue: number) {
+    this.buyValue = buyValue;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }
+
+  public setDoorsQty(doorsQty: number) {
+    this.doorsQty = doorsQty;
+  }
+
+  public getDoorsQty() {
+    return this.doorsQty;
+  }
+
+  public setSeatsQty(seatsQty: number) {
+    this.seatsQty = seatsQty;
+  }
+
+  public getSeatsQty() {
+    return this.seatsQty;
+  }
+}
+
+export default Car;
