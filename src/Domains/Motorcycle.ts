69e7709d1b99fe20c3b7653c504f4404ca90ba3c
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-23 23:24:01 -0300

    feat: Motorcycle Domain

---
+++ src/Domains/Motorcycle.ts
@@ -0,0 +1,100 @@
+import IMotorcycle from '../Interfaces/IMotorcycle';
+
+class Motorcycle {
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  private status?: boolean;
+  protected buyValue: number;
+  private category: string;
+  private engineCapacity: number;
+
+  constructor(
+    {
+      engineCapacity,
+      category,
+      buyValue,
+      status = false,
+      color,
+      year,
+      model,
+      id,
+    }: IMotorcycle,
+  ) {
+    this.id = id;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.status = status;
+    this.buyValue = buyValue;
+    this.category = category;
+    this.engineCapacity = engineCapacity;
+  }
+
+  public setId(id: string) {
+    this.id = id;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public setModel(model: string) {
+    this.model = model;
+  }
+
+  public getModel() {
+    return this.model;
+  }
+
+  public setYear(year: number) {
+    this.year = year;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+
+  public setColor(color: string) {
+    this.color = color;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public setStatus(status: boolean) {
+    this.status = status;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public setBuyValue(buyValue: number) {
+    this.buyValue = buyValue;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }
+
+  public setCategory(category: string) {
+    this.category = category;
+  }
+
+  public getCategory() {
+    return this.category;
+  }
+
+  public setEngineCapacity(engineCapacity: number) {
+    this.engineCapacity = engineCapacity;
+  }
+
+  public getEngineCapacity() {
+    return this.engineCapacity;
+  }
+}
+
+export default Motorcycle;
