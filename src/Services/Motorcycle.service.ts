ee9bc5e8b715a924595813ec9e8e22bcc27a0606
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-23 23:37:59 -0300

    feat: motorcycle find all and find by Id, service

--- src/Services/Motorcycle.service.ts
+++ src/Services/Motorcycle.service.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from 'mongoose';
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleModel from '../Models/Schemas/Motorcycle.model';
@@ -7,6 +8,21 @@ class MotorcycleService {
     const motorcycleODM = new MotorcycleModel();
     const newMotorcycle = await motorcycleODM.create(motorcycle);
     return new Motorcycle(newMotorcycle);
+  }
+
+  public async findAllMotorcycles() {
+    const motorcycleODM = new MotorcycleModel();
+    const motorcycles = await motorcycleODM.findAll();
+    const response = motorcycles.map((i) => new Motorcycle(i));
+    return response;
+  }
+
+  public async findMotorcycleById(id: string) {
+    const motorcycleODM = new MotorcycleModel();
+    if (!isValidObjectId(id)) return { status: 422, message: 'Invalid mongo id' };
+    const motorcycle = await motorcycleODM.findById(id);
+    if (!motorcycle) return { status: 404, message: 'Motorcycle not found' };
+    return { status: 200, response: new Motorcycle(motorcycle) };
   }
 }
 
